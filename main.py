7f3e35e65a220192a973fe38b86bf8be0bda3c2b
Vladimir Savelev <Era-gon2013@yandex.ru>
2022-12-14 16:24:30 +0600

    Why

--- main.py
+++ main.py
@@ -99,4 +99,4 @@ if __name__ == '__main__':
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
-    print(start_training(char_name, char_class)) 
+    print(start_training(char_name, char_class))
